f8f5ee8b773195a471bbb36516123d7523ea0f96
reviananda <99311022+reviananda@users.noreply.github.com>
2022-02-09 22:30:29 +0700

    Add files via upload

---
+++ pbo prak 1.py
@@ -0,0 +1,12 @@
+###rumus balok
+p = 4
+l = 9
+t = 6
+
+#rumus luas balok
+luas = ( 2 * p * l ) + ( 2 * p * t ) + ( 2 * l * t )
+print(luas)
+
+#rumus volume balok
+volume = p * l * t
+print(volume)
